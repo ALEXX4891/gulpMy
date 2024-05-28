838693bccf2dc9a08e3d46419dd0cf132bb77680
ALEXX4891 <alexx4891@mail.ru>
2024-05-28 15:08:43 +0500

    добавит gulp

---
+++ src/js/app.js
@@ -0,0 +1,24 @@
+// import * as flsFunctions from "./modules/functions.js";
+
+// flsFunctions.isWebp();
+
+//пример импорта библиотеки swiper из node_modules: 
+// import Swiper, { Navigation, Pagination } from "swiper";
+
+// const swiper = new Swiper(".swiper", {
+//   modules: [Navigation, Pagination],
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//     type: "bullets",
+//   },
+// })
+
+// new SimpleBar(document.querySelector(".table-body-wrapper"), {
+//   scrollbarMaxSize: 85,
+//   autoHide: false,
+//   forceVisible: true,
+// });
